9984ddd5759e0976d5295b267cf89e7f0b07b19b
arham <arham.aalam18@gmail.com>
2018-04-03 22:15:30 +0530

    update Main

--- Main.py
+++ Main.py
@@ -11,15 +11,20 @@ fgbg = cv2.createBackgroundSubtractorMOG2()
 fgbg2 = cv2.createBackgroundSubtractorMOG2()
 start = time.time() + 1
 x1, y1, w1, h2 = 0,10,10,10
+flag = False
 while True:
+    flag = False
     ret, frame = cap.read()
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
     for (x,y,w,h) in faces:
         cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = frame[y:y+h, x:x+w]
         [x1, y1, w1, h2] = [x,y,w,h]
-    width = cap.get(3)  # float
-    height = cap.get(4) # float
+        flag = True
+    width = cap.get(3) 
+    height = cap.get(4)
     fgmask = fgbg.apply(frame)
     fgmask = cv2.morphologyEx(fgmask, cv2.MORPH_OPEN, kernel)
     preimg = fgmask[0:int(y1//2), 0:int(width)]
@@ -32,9 +37,10 @@ while True:
     #cv2.imshow('frame',fgmask)
     #cv2.imshow('frame',fgmask2)
     cv2.putText(diff,str(diffsum), (10,20), cv2.FONT_HERSHEY_SIMPLEX, 2, 255)
-    cv2.imshow('diff',diff)
+    #cv2.imshow('diff',diff)
     cv2.imshow('frame',frame)
-    if diffsum//1000000 > 0:
+    #need to          this   | remove if dont work
+    if diffsum//1000000 > 0 and flag:
         if (int(time.time()-start) > 1):
             
             leftimg = diff[0:int(y1//2), 0:int(x1 + w1//2)]
@@ -46,8 +52,7 @@ while True:
                 pyautogui.press('left')
                 print("left pressed")
             start = time.time()
-    k = cv2.waitKey(30) & 0xff
-    if k == 27:
+    if cv2.waitKey(17) == ord('q'):
         break
     
 
